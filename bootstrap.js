db00f062991d3462a21ebc43728685b3536d4d17
Mydoge <LoganLMB2008@hotmail.com>
2019-05-08 20:09:43 -0400

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,1 +1,5 @@
-// Setting cookies
+// Setting cookies 
+document.cookie = "site=https://okhand.tk";
+document.cookie = "bootstrap=/bootstrap.js";
+document.cookie = "bootstrapstyle=/bootstrap.js";
+document.cookie = "meta=/meta.cfg";
